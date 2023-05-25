36182cbcd9b1b506bb806486f5cbfe18c44fb854
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-05-25 13:32:51 +0200

    Updated choose - html/css and js
    Fixed CSS flex and made it resposnive.
    Removed all forms but one from html.
    Rewrote js to be scalable/maintainable.

--- src/modules/choose.js
+++ src/modules/choose.js
@@ -1,111 +1,68 @@
-// Get references to the forms
-const form1 = document.getElementById('question1');
-const form2 = document.getElementById('question2');
-const form3 = document.getElementById('question3');
-const form4 = document.getElementById('question4');
-const form5 = document.getElementById('question5');
-const form6 = document.getElementById('question6');
+// Frågorna kan läggas in i databasen så slipper vi ha de här
+const dbQuestions = [
+    {
+        title: 'workplace',
+        question: 'Skulle du hellre arbeta...',
+        answer1: 'remote',
+        answer2: 'on_site'
+    },
+    {
+        title: 'size',
+        question: 'Arbeta på ett...',
+        answer1: 'startup',
+        answer2: 'korporat'
+    }
+];
 
-// User choices object
-let userChoices = {};
+const form = document.querySelector('form');
+const questionText = document.querySelector('h2');
+const answerText1 = document.getElementById('a1');
+const answerText2 = document.getElementById('a2');
+const answerRadios = Array.from(document.getElementsByName('answer'));
 
-// Process form 1
-form1.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedOccupation = Array.from(form1.elements.occupation)
-        .filter((checkbox) => checkbox.checked)
-        .map((checkbox) => checkbox.value);
-    userChoices['occupation'] = selectedOccupation;
-
-    // Hide first question, show second question
-    form1.classList.add('hide');
-    form2.classList.remove('hide');
-    console.log(userChoices)
-});
-
-// Process form 2
-form2.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedIndustry = Array.from(form2.elements.industry)
-        .filter((checkbox) => checkbox.checked)
-        .map((checkbox) => checkbox.value);
-    userChoices['industry'] = selectedIndustry;
-
-    // Hide second question, show third question
-    form2.classList.add('hide');
-    form3.classList.remove('hide');
-    console.log(userChoices)
-});
-
-// Process form 3
-form3.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedWorkplace;
-    const workplaceOptions = document.getElementsByName("workplace");
-    workplaceOptions.forEach(option => {
-        if (option.checked) {
-            selectedWorkplace = option.value;
-        }
-    });
-    userChoices['workplace'] = selectedWorkplace;
-
-    // Hide third question, show fourth question
-    form3.classList.add('hide');
-    form4.classList.remove('hide');
-    console.log(userChoices)
-});
-
-// Process form 4
-form4.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedCompanySize;
-    const sizeOptions = document.getElementsByName("size");
-    sizeOptions.forEach(option => {
-        if (option.checked) {
-            selectedCompanySize = option.value;
-        }
-    });
-    userChoices['size'] = selectedCompanySize;
+const userChoices = {};
+let iteration = 0;
 
 
-    form4.classList.add('hide');
-    form5.classList.remove('hide');
-    console.log(userChoices)
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    answerRadios[0].value = dbQuestions[iteration].answer1;
+    answerRadios[2].value = dbQuestions[iteration].answer2;
+    const selectedAnswer = answerRadios.find((radio) => radio.checked)?.value;
+
+    if (!selectedAnswer) {
+        console.log('Please select an answer');
+        return;
+    }
+
+    const { title } = dbQuestions[iteration];
+    userChoices[title] = selectedAnswer;
+    iteration++;
+
+    if (iteration === dbQuestions.length) {
+        cacheAnswers();
+    } else {
+        renderQuestion();
+    }
 });
 
 
-// Process form 5
-form5.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedCulture;
-    const cultureOptions = document.getElementsByName("culture");
-    cultureOptions.forEach(option => {
-        if (option.checked) {
-            selectedCulture = option.value;
-        }
-    });
-    userChoices['culture'] = selectedCulture;
-
-    // Hide fifth question, show the sixth question...
-    form5.classList.add('hide');
-    form6.classList.remove('hide');
-    console.log(userChoices)
-});
+// Renders the question
+const renderQuestion = () => {
+    const { question, answer1, answer2 } = dbQuestions[iteration];
+    questionText.innerText = question;
+    answerText1.innerText = answer1;
+    answerText2.innerText = answer2;
+};
 
 
-// Process form 6
-form6.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-    let selectedLocation;
-    const locationOptions = document.getElementsByName("location");
-    locationOptions.forEach(option => {
-        if (option.checked) {
-            selectedLocation = option.value;
-        }
-    });
-    userChoices['location'] = selectedLocation;
-    console.log(userChoices); // will print final user choices
+// Caches the answers and redirects the user to forForetag.html
+const cacheAnswers = () => {
+    console.log(userChoices);
     localStorage.setItem('userChoices', JSON.stringify(userChoices));
-    window.location.href = "forForetag.html"; // Redirects to forForetag.html
-});
+    window.location.href = 'forForetag.html';
+};
 
+
+// Render first question
+renderQuestion();
