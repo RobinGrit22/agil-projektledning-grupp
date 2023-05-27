ae9d447282100d1a9de7e9a9995befa106a61436
Eleonora Nocentini Sköldebrink <eleonora.nocentini@gmail.com>
2023-05-27 12:26:52 +0200

    bytt så att man får alla företag, ej endast remote

--- src/index.js
+++ src/index.js
@@ -32,15 +32,38 @@ setDoc(doc(db, "companies", "company13"), {
 
 
 //sortera företagen
-async function getRemoteStartups() {
+async function getCompanies() {
   const companiesRef = collection(db, "companies");
 
-  const q = query(companiesRef, where("workplace", "==", "remote"), where("size", "==", "startup"));
+  //tom array som håller alla företagen
+  const companyQueries = [];
 
-  const querySnapshot = await getDocs(q);
-  querySnapshot.forEach((doc) => {
-    console.log(doc.id, " => ", doc.data());
+  //fetchar företagen beroende på användarens val
+  const workplaces = ["remote", "onsite", "do not know"];
+  const sizes = ["startup", "corporate", " do not know"];
+
+  for(let workplace of workplaces){
+    for(let size of sizes){
+      const q = query(companiesRef, where("workplace", "==", "workplace"), where("size", "==", "size"));
+      companyQueries.push(getDocs(q));
+    }
+  }
+
+  //väntar på alla queries (kan ta tid om vi har många företag i db)
+  const allSnapshots =  await Promise.all(companyQueries);
+
+  //kombinera resultatet utifrån användarens val
+  let allCompanies = [];
+  for (let snapshot of allSnapshots){
+    const companies = snapshot.docs.map(doc => ({...doc.data(), id: doc.id}));
+    allCompanies = [...allCompanies, ...companies];
+  }
+
+  // const querySnapshot = await getDocs(q);
+
+  allCompanies.forEach((company) => {
+    console.log(company.id, " => ", company);
   });
 }
 
-getRemoteStartups();
+getCompanies();
