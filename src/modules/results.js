5d1c55e1101739b7367eff00bf2133524d55e3f9
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-05-31 11:52:41 +0200

    Added some lorem ipsum to description of each company

--- src/modules/results.js
+++ src/modules/results.js
@@ -25,7 +25,15 @@ function displayCompanies(companies) {
   companies.forEach((company) => {
     const companyDiv = document.createElement("div");
     companyDiv.classList.add("white-container");
-    companyDiv.textContent = company.name;
+    const companyHeader = document.createElement("h3");
+    companyHeader.innerText = company.name;
+    companyDiv.appendChild(companyHeader);
+
+    const companyInfo = document.createElement("p");
+    companyInfo.innerText =
+      '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."';
+
+    companyDiv.appendChild(companyInfo);
     container.appendChild(companyDiv);
   });
 }
