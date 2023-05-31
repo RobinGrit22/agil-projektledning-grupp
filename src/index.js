89a5c2991401267f697ded14d8d11ec75a0c5b52
Eddie Peters <112489365+eddiepeters00@users.noreply.github.com>
2023-05-31 10:56:04 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,1 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase, ref, set, onValue } from "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyA0tY2Svlzr0j8F6Dv0WSXeIrsG6F_cY3I",
-  authDomain: "agil-projektledning-grupp.firebaseapp.com",
-  databaseURL:
-    "https://agil-projektledning-grupp-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "agil-projektledning-grupp",
-  storageBucket: "agil-projektledning-grupp.appspot.com",
-  messagingSenderId: "926760801750",
-  appId: "1:926760801750:web:e5f6e20c18babccafec077",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
